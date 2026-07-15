44513b1d36e863576f4f3904110476f47f48ea8b
Yashraj Singh <yashraj.rajawat10@gmail.com>
2026-07-15 14:14:41 +0530

    fixed config.py

--- ms2-speech-agent/app/config.py
+++ ms2-speech-agent/app/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict  # type: ignore
 from dotenv import load_dotenv
 
 load_dotenv()
